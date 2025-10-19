debd679346db52e6da807548f47b829cf7f529ef
StepbyStepper <gorbunov.dv.webdev@gmail.com>
2025-10-19 10:11:21 +0300

    Исправлена логика работы функции download_image

--- utils.py
+++ utils.py
@@ -14,13 +14,16 @@ def get_filename_from_url(url):
     return unquote(filename)
 
 def download_image(image_url: str, save_path: str):
+    """Скачивает изображение по URL с обработкой ошибок"""
     os.makedirs(os.path.dirname(save_path), exist_ok=True)
-    response = requests.get(image_url, stream=True)
-
-    if not response.ok:
-        with open(save_path, "wb") as file:
-            for chunk in response.iter_content(1024):
-                file.write(chunk)
-        print(f"Сохранено: {save_path}")
-    else:
-        print(f"Ошибка загрузки {image_url}: {response.status_code}")
+    try:
+        response = requests.get(image_url, stream=True, timeout=15)
+        if response.ok:
+            with open(save_path, "wb") as file:
+                for chunk in response.iter_content(1024):
+                    file.write(chunk)
+            print(f"Сохранено: {save_path}")
+        else:
+            print(f"Ошибка загрузки {image_url}: {response.status_code}")
+    except requests.exceptions.RequestException as e:
+        print(f"Ошибка при скачивании {image_url}: {e}")
